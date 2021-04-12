20d2c44603439f3a90c9a617ddad9c6e937f81f1
Sárdi András <sardi.andras@gmail.com>
2021-04-12 10:50:04 +0200

    Create Zombi.java

---
+++ Zombi.java
@@ -0,0 +1,13 @@
+public class Zombi {
+
+    private int elet;
+
+    public int getElet() {
+        return elet;
+    }
+
+    public void setElet(int elet) {
+        this.elet = elet;
+    }
+
+}
